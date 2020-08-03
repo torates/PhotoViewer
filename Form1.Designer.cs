b6a9ffd68a1cac15574da9a6d36d59646b22380a
Andrés Mateo La Salle <UO279132@uniovi.es>
2020-08-03 21:00:11 +0200

    Main commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PhotoViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.info = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 423);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            this.pictureBox1.DragLeave += new System.EventHandler(this.pictureBox1_DragLeave);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.info});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // info
+            // 
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(40, 20);
+            this.info.Text = "info";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "space";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem info;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Text;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+
+
+namespace PhotoViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            this.info.Visible = false;
+            this.info.Enabled = false;
+            this.pictureBox1.MouseWheel += PictureBox1_MouseWheel;
+            ControlExtension.Draggable(pictureBox1, true);
+            this.AllowDrop = true;
+            this.pictureBox1.AllowDrop = true;
+            try
+            {
+                if (args.Length > 0)
+                {
+                    dragPath = args[0];
+                    hasDragged = true;
+                }
+                if (hasDragged == true)
+                {
+                    string img;
+                    img = dragPath;
+                    path = img;
+                    pictureBox1.Image = Image.FromFile(img);
+                    pictureBox1.Image = checkImgSize(path);
+                    this.pictureBox1.Width = this.pictureBox1.Image.Width;
+                    this.pictureBox1.Height = this.pictureBox1.Image.Height;
+                    Size finalSize = new Size(this.pictureBox1.Image.Width + 17, this.pictureBox1.Image.Height + 67);
+                    Point zero = new Point(0, 24);
+                    this.Size = finalSize;
+                    this.MinimumSize = new Size(this.pictureBox1.Width, this.pictureBox1.Height + 66);
+                    pictureBox1.Location = zero;
+                    filename = Path.GetFileName(img);
+                    this.Text = filename;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                hasDragged = false;
+                MessageBox.Show("IndexOutOfRangeException");
+            }
+
+        }
+        OpenFileDialog abrir = new OpenFileDialog();
+        string path;
+        string dragPath;
+        bool hasDragged;
+        string filename;
+        int counter;
+        int desiredWidth;
+        private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            Image img = Image.FromFile(path);
+            decimal width = img.Width / 20;
+            decimal height = img.Height / 20;
+            int minimumSize = pictureBox1.Image.Width + pictureBox1.Image.Height / 2;
+            if (e.Delta > 0)
+            {
+                pictureBox1.Width = pictureBox1.Width + Convert.ToInt32(width);
+                pictureBox1.Height = pictureBox1.Height + Convert.ToInt32(height);
+                Bitmap output = new Bitmap(img, pictureBox1.Width + Convert.ToInt32(width), pictureBox1.Height + Convert.ToInt32(height));
+                Image outputf = (Image)output;
+                pictureBox1.Image = outputf;
+                counter -= 1;
+
+
+            }
+            else if (e.Delta < 0)
+            {
+                if (counter != 8)
+                {
+                    pictureBox1.Width = pictureBox1.Width - Convert.ToInt32(width);
+                    pictureBox1.Height = pictureBox1.Height - Convert.ToInt32(height);
+                    Bitmap output = new Bitmap(img, pictureBox1.Width - Convert.ToInt32(width), pictureBox1.Height - Convert.ToInt32(height));
+                    Image outputf = (Image)output;
+                    pictureBox1.Image = outputf;
+                    counter += 1;
+                }
+
+            }
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string img;
+            try
+            {
+                if (abrir.ShowDialog() == DialogResult.OK)
+                {
+                    counter = 0;
+                    img = abrir.FileName;
+                    path = img;
+                    pictureBox1.Image = Image.FromFile(img);
+                    pictureBox1.Image = checkImgSize(path);
+                    this.pictureBox1.Width = this.pictureBox1.Image.Width;
+                    this.pictureBox1.Height = this.pictureBox1.Image.Height;
+                    Size finalSize = new Size(this.pictureBox1.Image.Width + 17, this.pictureBox1.Image.Height + 67);
+                    Point zero = new Point(0, 24);
+                    this.Size = finalSize;
+                    this.MinimumSize = new Size(this.pictureBox1.Width, this.pictureBox1.Height + 66);
+                    pictureBox1.Location = zero;
+                    filename = Path.GetFileName(img);
+                    this.Text = filename;
+                }
+                else if (hasDragged == true)
+                {
+                }
+            }
+            catch
+            {
+                string msg = "Only input pictures or gifs!";
+                string caption = "ERROR, WRONG FILE EXTENSION";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(msg, caption, buttons);
+                if (result == DialogResult.OK)
+                {
+                    openToolStripMenuItem_Click(sender, e);
+                }
+            }
+        }
+        Label dropToLabel;
+        public Image checkImgSize(string path)
+        {
+            Size desiredSize = new Size(1366, 768);
+            Image img = Image.FromFile(path);
+            desiredWidth = desiredSize.Width;
+            decimal ratio = Convert.ToDecimal(img.Width) / Convert.ToDecimal(img.Height);
+            if (pictureBox1.Image.Width > desiredSize.Width)
+            {
+                decimal width = desiredSize.Height * ratio;
+                Bitmap output = new Bitmap(img, Convert.ToInt32(width), desiredSize.Height);
+                Image outputf = (Image)output;
+                return outputf;
+            }
+            else if (pictureBox1.Image.Height > desiredSize.Height)
+            {
+                decimal height = desiredSize.Height * ratio;
+                Bitmap output = new Bitmap(img, Convert.ToInt32(height), desiredSize.Height);
+                Image outputf = (Image)output;
+                return outputf;
+            }
+            return img;
+
+        }
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Process.Start("explorer.exe", "/select, " + path);
+        }
+
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                Image img = Image.FromFile(files[0]);
+                path = files[0];
+                pictureBox1.Image = img;
+                pictureBox1.Image = checkImgSize(files[0]);
+                this.Controls.Remove(dropToLabel);
+                dropToLabel.Enabled = false;
+                dropToLabel.Visible = false;
+            }
+            catch
+            {
+                string msg = "Only input pictures or gifs!";
+                string caption = "ERROR, WRONG FILE EXTENSION";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(msg, caption, buttons);
+                if (result == DialogResult.OK)
+                {
+                    dropToLabel.Enabled = false;
+                    dropToLabel.Visible = false;
+                    return;
+                }
+            }
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+            dropToLabel = new Label();
+            dropToLabel.Text = "Drop here to open the file";
+            dropToLabel.Font = new Font(FontFamily.GenericMonospace, 20);
+            dropToLabel.AutoSize = true;
+            dropToLabel.Location = new Point(this.Width / 2, this.Height / 2);
+
+            this.Controls.Add(dropToLabel);
+            dropToLabel.BringToFront();
+        }
+
+        private void pictureBox1_DragLeave(object sender, EventArgs e)
+        {
+            this.Controls.Remove(dropToLabel);
+            dropToLabel.Enabled = false;
+            dropToLabel.Visible = false;
+        }
+    }
+}
